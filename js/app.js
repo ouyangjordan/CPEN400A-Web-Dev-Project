9d4bc364b9979361ccdf4569c8f476c83c12cccf
Noor <noor_khan@alumni.ubc.ca>
2019-09-26 18:53:45 -0700

    done except for the last question

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
-/*
-* Constructor function for creating a store object
-* Store object will keep track of the items in the store and the items in the cart
-*/
-const Store = function (initialStock){
-    this.stock = initialStock;
-    this.cart = {};
-    this.addItemToCart = function (itemName) {console.log(itemName)}
-    this.removeItemFromCart = function (itemName) {console.log(itemName)}
-}
-
 /* global variable named products and then assign it an associative array (i.e. an object) that will store information about the products */
 let products = {
     "Box1": {
@@ -65,7 +54,53 @@ let products = {
         "price" :  100,
         "quantity" : 5
     }
+};
+
+/*
+* Constructor function for creating a store object
+* Store object will keep track of the items in the store and the items in the cart
+*/
+const Store = function (initialStock){
+    this.stock = initialStock;
+    this.cart = {};
+    this.addItemToCart = (itemName) => {
+        if(this.stock[itemName].quantity > 0)
+        {
+            if(this.cart[itemName])
+                this.cart[itemName]++;
+            else
+                this.cart[itemName] = 1;
+            this.stock[itemName].quantity--;
+        }
+        else
+        {
+            alert(`${products[itemName].label} is out of stock. Sorry!`);
+        }
+    }
+    this.removeItemFromCart = (itemName) => {
+        if(this.cart[itemName]){
+            this.cart[itemName]--;
+            if(this.cart[itemName] === 0) delete this.cart[itemName];
+            this.stock[itemName].quantity++;
+        }
+        else{
+            alert(`${products[itemName].label} is not in your cart. Did you mean to add it?`)
+        }
+    }
 }
 
 /* Create a global variable named store */
 let store = new Store(products);
+
+/* Implement a function with the signature showCart(cart), which invokes an alert to display the contents of the given cart object */
+function showCart(cart) {
+    let res = [];
+    for (let key in cart) {
+        if (cart.hasOwnProperty(key))
+            res.push(`${key} : ${cart[key]}`);
+    }
+    if(res.length === 0)
+        alert("Your cart is currently empty!")
+    else
+        alert(`Your cart currently has the following items:\n${res.join('\n')}`);
+}
